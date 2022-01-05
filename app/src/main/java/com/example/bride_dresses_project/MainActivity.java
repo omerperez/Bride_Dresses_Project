d20bfc56a0da3e88bc77c0a4ee5e64e7f8bea08f
Omer Perez <omerperez222@gmail.com>
2022-01-05 18:32:30 +0200

    Change location of \

--- app/src/main/java/com/example/bride_dresses_project/MainActivity.java
+++ app/src/main/java/com/example/bride_dresses_project/MainActivity.java
@@ -16,22 +16,26 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.bride_dresses_project.databinding.ActivityMainBinding;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
 
     private AppBarConfiguration mAppBarConfiguration;
     private ActivityMainBinding binding;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
         setSupportActionBar(binding.appBarMain.toolbar);
-        binding.appBarMain.fab.setOnClickListener(new View.OnClickListener() {
+        binding.appBarMain.fab.setOnClickListener(new View.OnClickListener()
+        {
             @Override
-            public void onClick(View view) {
+            public void onClick(View view)
+            {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
