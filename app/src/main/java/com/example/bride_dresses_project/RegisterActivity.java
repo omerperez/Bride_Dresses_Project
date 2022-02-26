ba4a5a778294aaa03d19e8c77f21f962f9f75c00
OmerPerez <omerperez222@gmail.com>
2022-02-26 15:49:50 +0200

    Add profile image to upload in the register design

--- app/src/main/java/com/example/bride_dresses_project/RegisterActivity.java
+++ app/src/main/java/com/example/bride_dresses_project/RegisterActivity.java
@@ -1,20 +1,45 @@
 package com.example.bride_dresses_project;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.example.bride_dresses_project.databinding.ActivityMainBinding;
+import com.example.bride_dresses_project.databinding.ActivityRegisterBinding;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    ActivityRegisterBinding binding;
+    Uri imageUri;
+    StorageReference storageReference;
+    ProgressDialog pd;
 
     EditText fullName, phone, password, confirmPassword;
     Button registerBtn;
@@ -26,6 +51,18 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        binding = ActivityRegisterBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        binding.registerAddImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                selectImage();
+            }
+        });
+
+
+
         fullName = findViewById(R.id.register_full_name);
         phone = findViewById(R.id.register_phone);
         password = findViewById(R.id.register_password);
@@ -55,6 +92,7 @@ public class RegisterActivity extends AppCompatActivity {
                             } else {
                                 databaseReference.child("users").child(phoneTxt).child("fullName").setValue(fullNameTxt);
                                 databaseReference.child("users").child(phoneTxt).child("password").setValue(passwordTxt);
+                                uploadImageToFirebase();
                                 Toast.makeText(RegisterActivity.this, "User Registered successfully", Toast.LENGTH_SHORT).show();
                             }
                         }
@@ -76,5 +114,51 @@ public class RegisterActivity extends AppCompatActivity {
             }
         });
     }
+
+    private void selectImage() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent, 100);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == 100 && requestCode == RESULT_OK && data != null && data.getData() != null){
+            imageUri = data.getData();
+            binding.registerAddImage.setImageURI(imageUri);
+        }
+    }
+
+    private void uploadImageToFirebase() {
+        pd = new ProgressDialog(this);
+        pd.setTitle("Uploaded File...");
+        pd.show();
+
+        SimpleDateFormat format = new SimpleDateFormat("yyy_MM_dd_HH_mm_ss", Locale.CANADA);
+        Date now = new Date();
+        String fileName = format.format(now);
+
+        storageReference = FirebaseStorage.getInstance().getReference("images/" + fileName);
+        storageReference.putFile(imageUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                Toast.makeText(RegisterActivity.this, "Successfully Uploaded", Toast.LENGTH_SHORT).show();
+                if(pd.isShowing()){
+                    pd.dismiss();
+                }
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                if(pd.isShowing()){
+                    pd.dismiss();
+                }
+                Toast.makeText(RegisterActivity.this, "Failed to Upload", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
 }
 
