2b2345f32a691c5424ec19108412c3c79001e6a2
linoybekker <linoylbekker@gmail.com>
2022-03-18 15:45:02 +0200

    Update dress description page

--- app/src/main/java/com/example/bride_dresses_project/fragments/dresses/DressDescriptionFragment.java
+++ app/src/main/java/com/example/bride_dresses_project/fragments/dresses/DressDescriptionFragment.java
@@ -5,15 +5,20 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.bride_dresses_project.R;
 import com.example.bride_dresses_project.model.Dress;
+import com.google.android.material.button.MaterialButton;
 import com.google.gson.Gson;
+import com.squareup.picasso.Picasso;
 
 
 public class DressDescriptionFragment extends Fragment {
@@ -38,7 +43,25 @@ public class DressDescriptionFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_dress_description, container, false);
+        View view= inflater.inflate(R.layout.fragment_dress_description, container, false);
+        TextView dressType=view.findViewById(R.id.dressItemType);
+        TextView dressDesigner=view.findViewById(R.id.dressItemDesigner);
+        TextView dressPrice=view.findViewById(R.id.dressItemPrice);
+        ImageView dressImage=view.findViewById(R.id.dressItemImage);
+        MaterialButton editButton=view.findViewById(R.id.dress_description_edit_button);
+        MaterialButton deleteButton=view.findViewById(R.id.dress_description_delete_button);
+        MaterialButton backButton=view.findViewById(R.id.dress_description_back_button);
+
+        dressType.setText(mDress.getType());
+        dressPrice.setText(mDress.getPrice());
+       // dressDesigner.setText(mDress.getUser().getFullName());
+        Picasso.get().load(mDress.getImageUrl()).into(dressImage);
+
+        backButton.setOnClickListener(view1 -> {
+            Navigation.findNavController(getView()).navigate(R.id.action_dressDescriptionFragment_to_dressesListFragment);
+
+        });
+        return  view;
     }
 
 
