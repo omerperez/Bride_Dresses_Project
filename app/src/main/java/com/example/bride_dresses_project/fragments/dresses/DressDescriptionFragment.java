99d9c8b9ead11175a82a676a14a5ddc5442c24ad
linoybekker <linoylbekker@gmail.com>
2022-03-22 15:49:14 +0200

    Update DressDescriptionFragment and DressesListFragment

--- app/src/main/java/com/example/bride_dresses_project/fragments/dresses/DressDescriptionFragment.java
+++ app/src/main/java/com/example/bride_dresses_project/fragments/dresses/DressDescriptionFragment.java
@@ -14,6 +14,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.bride_dresses_project.R;
+import com.example.bride_dresses_project.model.AppLocalDb;
 import com.example.bride_dresses_project.model.entities.Dress;
 import com.example.bride_dresses_project.model.Model;
 import com.google.android.material.button.MaterialButton;
@@ -24,7 +25,6 @@ import com.squareup.picasso.Picasso;
 public class DressDescriptionFragment extends Fragment {
 
     public static final String DRESS_PARAM = "Dress";
-    private Dress mDress;
     MaterialButton backButton;
     MaterialButton editButton;
     MaterialButton deleteButton;
@@ -32,65 +32,37 @@ public class DressDescriptionFragment extends Fragment {
 
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            String stringObjectDress = getArguments().getString(DRESS_PARAM);
-            Gson g = new Gson();
-            mDress = g.fromJson(stringObjectDress,Dress.class);
-        }
-    }
-
-    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view= inflater.inflate(R.layout.fragment_dress_description, container, false);
-        TextView dressType=view.findViewById(R.id.dressItemType);
-        TextView dressDesigner=view.findViewById(R.id.dressItemDesigner);
-        TextView dressPrice=view.findViewById(R.id.dressItemPrice);
-        ImageView dressImage=view.findViewById(R.id.dressItemImage);
-        editButton=view.findViewById(R.id.dress_description_page_edit_button);
-         deleteButton=view.findViewById(R.id.dress_description_page_delete_button);
-         backButton=view.findViewById(R.id.dress_description_back_button);
+        View view = inflater.inflate(R.layout.fragment_dress_description, container, false);
+        Dress dress = DressDescriptionFragmentArgs.fromBundle(getArguments()).getDress();
+        TextView dressType = view.findViewById(R.id.dressItemType);
+        TextView dressDesigner = view.findViewById(R.id.dressItemDesigner);
+        TextView dressPrice = view.findViewById(R.id.dressItemPrice);
+        ImageView dressImage = view.findViewById(R.id.dressItemImage);
+        editButton = view.findViewById(R.id.dress_description_page_edit_button);
+        deleteButton = view.findViewById(R.id.dress_description_page_delete_button);
+        backButton = view.findViewById(R.id.dress_description_back_button);
         navController = NavHostFragment.findNavController(this);
-        dressType.setText(mDress.getType());
-        dressPrice.setText(mDress.getPrice());
-       // dressDesigner.setText(mDress.getUser().getFullName()); TODO
-        Picasso.get().load(mDress.getImageUrl()).into(dressImage);
-        setBackButtonOnClickListener();
+        dressType.setText(dress.getType());
+        dressPrice.setText(dress.getPrice());
+        // dressDesigner.setText(mDress.getUser().getFullName()); TODO
+        Picasso.get().load(dress.getImageUrl()).into(dressImage);
         setDeleteButtonOnClickListener();
-
-        return  view;
+        return view;
     }
 
-        private void setBackButtonOnClickListener() {
-            backButton.setOnClickListener(view1 -> {
-                Navigation.findNavController(getView()).navigate(R.id.action_dressDescriptionFragment_to_dressesListFragment);
-            });
-        }
 
     private void setDeleteButtonOnClickListener() {
         deleteButton.setOnClickListener(view -> {
             editButton.setEnabled(false);
             deleteButton.setEnabled(false);
-            Model.instance.deleteDress(mDress, () -> {
-                navController.navigateUp();
-            });
+            new Thread(() -> {
+                Dress dress = DressDescriptionFragmentArgs.fromBundle(getArguments()).getDress();
+                AppLocalDb.db.dressDao().delete(dress);
+                Model.instance.deleteDress(dress, () -> navController.navigateUp());
+            }).start();
         });
     }
 
-//    private void setEditButtonOnClickListener() {
-//        editButton.setOnClickListener(view -> {
-//            navController.navigate(DressDescriptionFragment.DRE   .actionReviewDetailsFragmentToAddReviewFragment(true));
-//        });
-//    }
-//    @Override
-//    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-//        super.onViewCreated(view, savedInstanceState);
-//
-//        if(mDress!=null) {
-//            TextView tv = view.findViewById(R.id.dressItemType);
-//            tv.setText(mDress.getType());
-//        }
-//    }
 }
--- app/src/main/java/com/example/bride_dresses_project/fragments/dresses/DressesListFragment.java
+++ app/src/main/java/com/example/bride_dresses_project/fragments/dresses/DressesListFragment.java
@@ -1,34 +1,35 @@
 package com.example.bride_dresses_project.fragments.dresses;
+
 import android.os.Bundle;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
+
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.TextView;
+
 import com.example.bride_dresses_project.R;
+import com.example.bride_dresses_project.adapters.DressListAdapter;
 import com.example.bride_dresses_project.model.entities.Dress;
 import com.example.bride_dresses_project.model.Model;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.gson.Gson;
-import com.squareup.picasso.Picasso;
 
 
-public class DressesListFragment extends Fragment {
+public class DressesListFragment extends Fragment implements DressListAdapter.OnItemClickListener {
 
-    DressesListRvViewModel viewModel;
-    MyAdapter adapter;
-    SwipeRefreshLayout swipeRefresh;
-    FloatingActionButton addDressButton;
+    private DressesListRvViewModel viewModel;
+    private DressListAdapter mAdapter;
+    private SwipeRefreshLayout swipeRefresh;
+    private FloatingActionButton addDressButton;
 
     @Override
     public void onAttach(@NonNull Context context) {
@@ -44,106 +45,25 @@ public class DressesListFragment extends Fragment {
         addDressButton.setOnClickListener((view1 -> Navigation.findNavController(view).navigate(R.id.action_dressesListFragment_to_createDressFragment)));
         swipeRefresh = view.findViewById(R.id.dresseslist_swiperefresh);
         swipeRefresh.setOnRefreshListener(() -> Model.instance.refreshDressList());
-        RecyclerView list = view.findViewById(R.id.dressRv);
-        list.setHasFixedSize(true);
-        list.setLayoutManager(new LinearLayoutManager(getContext()));
-        adapter = new MyAdapter();
-        list.setAdapter(adapter);
-        adapter.setOnItemClickListener(new OnItemClickListener() {
-            @Override
-            public void onItemClick(View v, int position) {
-                Dress dress = viewModel.getData().getValue().get(position);
-                Gson g = new Gson();
-                String stringFromObject = g.toJson(dress);
-                Bundle b = new Bundle();
-                b.putString(DressDescriptionFragment.DRESS_PARAM,stringFromObject);
-                Navigation.findNavController(v).navigate(R.id.action_dressesListFragment_to_dressDescriptionFragment,b);
-            }
-        });
-        viewModel.getData().observe(getViewLifecycleOwner(), list1 -> refresh());
+        RecyclerView recyclerView = view.findViewById(R.id.dressRv);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        mAdapter = new DressListAdapter(getActivity(), this);
+        recyclerView.setAdapter(mAdapter);
+        viewModel.getAllDresses().observe(getViewLifecycleOwner(), dresses -> mAdapter.differ.submitList(dresses));
         swipeRefresh.setRefreshing(Model.instance.getDressListLoadingState().getValue() == Model.DressListLoadingState.loading);
-        Model.instance.getDressListLoadingState().observe(getViewLifecycleOwner(), dressListLoadingState -> {
-            if (dressListLoadingState == Model.DressListLoadingState.loading) {
-                swipeRefresh.setRefreshing(true);
-            } else {
-                swipeRefresh.setRefreshing(false);
-            }
-
-        });
+        Model.instance.getDressListLoadingState().observe(getViewLifecycleOwner(), dressListLoadingState -> swipeRefresh.setRefreshing(dressListLoadingState == Model.DressListLoadingState.loading));
         return view;
     }
 
-
-    private void refresh() {
-        adapter.notifyDataSetChanged();
+    @Override
+    public void onResume() {
+        super.onResume();
+        viewModel.refreshDressList();
     }
 
-    class MyViewHolder extends RecyclerView.ViewHolder {
-        ImageView image;
-        TextView price, type;
-        FloatingActionButton addBtn;
-        RecyclerView rvDresses;
-        Button dressItemMoreDetails;
-
-        public MyViewHolder(@NonNull View itemView, OnItemClickListener listener) {
-            super(itemView);
-            type = itemView.findViewById(R.id.dressItemType);
-            price = itemView.findViewById(R.id.dressItemPrice);
-            image = itemView.findViewById(R.id.dressItemImage);
-            dressItemMoreDetails= itemView.findViewById(R.id.dressItemMoreDetails);
-            dressItemMoreDetails.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    int pos = getAdapterPosition();
-                    listener.onItemClick(v, pos);
-                }
-            });
-        }
-
-        void bind(Dress dress) {
-            type.setText(dress.getType());
-            price.setText(dress.getPrice());
-            image.setImageResource(R.drawable.dress_icon);
-            if (dress.getImageUrl() != null && !dress.getImageUrl().isEmpty()) {
-                Picasso.get()
-                        .load(dress.getImageUrl())
-                        .into(image);
-            }
-        }
-    }
-
-    interface OnItemClickListener {
-        void onItemClick(View v, int position);
-    }
-
-    class
-    MyAdapter extends RecyclerView.Adapter<MyViewHolder> {
-        OnItemClickListener listener;
-
-        public void setOnItemClickListener(OnItemClickListener listener) {
-            this.listener = listener;
-        }
-
-        @NonNull
-        @Override
-        public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-            View view = getLayoutInflater().inflate(R.layout.dress_item, parent, false);
-            MyViewHolder holder = new MyViewHolder(view, listener);
-            return holder;
-        }
-
-        @Override
-        public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
-            Dress dress = viewModel.getData().getValue().get(position);
-            holder.bind(dress);
-        }
-
-        @Override
-        public int getItemCount() {
-            if (viewModel.getData().getValue() == null) {
-                return 0;
-            }
-            return viewModel.getData().getValue().size();
-        }
+    @Override
+    public void onItemClick(Dress dress) {
+        NavHostFragment.findNavController(this).navigate(DressesListFragmentDirections.actionDressesListFragmentToDressDescriptionFragment(dress));
     }
 }
