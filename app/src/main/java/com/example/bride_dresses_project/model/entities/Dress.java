bb018d995946fab46f5873c47c86383e3e55acd4
linoybekker <linoylbekker@gmail.com>
2022-03-22 15:45:39 +0200

    Updated dress model

--- app/src/main/java/com/example/bride_dresses_project/model/entities/Dress.java
+++ app/src/main/java/com/example/bride_dresses_project/model/entities/Dress.java
@@ -1,56 +1,76 @@
 package com.example.bride_dresses_project.model.entities;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import androidx.annotation.NonNull;
 import androidx.room.Entity;
 import androidx.room.PrimaryKey;
+
 import com.google.firebase.Timestamp;
 import com.google.firebase.firestore.FieldValue;
+
+import java.sql.Time;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 
 
 @Entity
-public class Dress
-{
-    public static final String DRESS_COLLECTION_NAME ="Dresses" ;
+public class Dress implements Parcelable {
+    public static final String DRESS_COLLECTION_NAME = "Dresses";
     @PrimaryKey
-   @NonNull
+    @NonNull
     private String id;
     private String price;
-    private  String imageUrl;
-    private  String type;
+    private String imageUrl;
+    private String type;
     private Long updateDate;
     private String ownerId;
-    private  boolean deleted;
+    private boolean deleted;
 
-    public Dress()
-    {
+    public Dress() {
         this.id = UUID.randomUUID().toString();
         type = "";
         price = "";
         imageUrl = "";
     }
-    public Dress(String type,String price,String imageUrl)
-    {
+
+    public Dress(String type, String price, String imageUrl) {
         this.id = UUID.randomUUID().toString();
-        this.type=type;
-        this.price=price+"$";
-        this.imageUrl=imageUrl;
-        this.updateDate=System.currentTimeMillis();
+        this.type = type;
+        this.price = price + "$";
+        this.imageUrl = imageUrl;
+        this.updateDate = System.currentTimeMillis();
     }
 
-    public Dress(String type,String price, String id, Boolean deleted) {
+    public Dress(String type, String price, String id, Boolean deleted) {
         this.type = type;
-        this.price=price;
+        this.price = price;
         this.id = id;
         this.deleted = deleted;
     }
 
-    public  String getId() {
+    protected Dress(Parcel in) {
+        id = in.readString();
+        price = in.readString();
+        imageUrl = in.readString();
+        type = in.readString();
+        if (in.readByte() == 0) {
+            updateDate = null;
+        } else {
+            updateDate = in.readLong();
+        }
+        ownerId = in.readString();
+        deleted = in.readByte() != 0;
+    }
+
+
+    public String getId() {
         return id;
     }
 
-    public void setId( String id) {
+    public void setId(String id) {
         this.id = id;
     }
 
@@ -102,14 +122,14 @@ public class Dress
         this.deleted = deleted;
     }
 
-    public  Map<String, Object> toJson() {
+    public Map<String, Object> toJson() {
         Map<String, Object> json = new HashMap<String, Object>();
-        json.put("id",id);
-        json.put("type",type);
-        json.put("price",price);
-        json.put("deleted",deleted);
+        json.put("id", id);
+        json.put("type", type);
+        json.put("price", price);
+        json.put("deleted", deleted);
         json.put("updateDate", FieldValue.serverTimestamp());
-        json.put("imageUrl",imageUrl);
+        json.put("imageUrl", imageUrl);
         return json;
     }
 
@@ -118,10 +138,15 @@ public class Dress
         String type = (String) json.get("type");
         String price = (String) json.get("price");
         Boolean deleted = (Boolean) json.get("deleted");
-        Timestamp ts = (Timestamp)json.get("updateDate");
-        Long updateDate = ts.getSeconds();
-        String imageUrl = (String)json.get("imageUrl");
-        Dress dress = new Dress(type,price,id,deleted);
+        Long updateDate = 0L;
+        try {
+            Timestamp ts = (Timestamp) json.get("updateDate");
+             updateDate = ts.getSeconds();
+        }catch (Exception exception){
+
+        }
+        String imageUrl = (String) json.get("imageUrl");
+        Dress dress = new Dress(type, price, id, deleted);
         dress.setUpdateDate(updateDate);
         dress.setImageUrl(imageUrl);
         return dress;
@@ -135,9 +160,43 @@ public class Dress
         result.put("imageUrl", imageUrl);
         result.put("deleted", deleted);
         result.put("ownerId", ownerId);
-        result.put("updateDate",updateDate);
+        result.put("updateDate", updateDate);
         return result;
     }
-    //---------------------------------//
+
+
+    public static final Creator<Dress> CREATOR = new Creator<Dress>() {
+        @Override
+        public Dress createFromParcel(Parcel in) {
+            return new Dress(in);
+        }
+
+        @Override
+        public Dress[] newArray(int size) {
+            return new Dress[size];
+        }
+    };
+
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+        parcel.writeString(id);
+        parcel.writeString(price);
+        parcel.writeString(imageUrl);
+        parcel.writeString(type);
+        if (updateDate == null) {
+            parcel.writeByte((byte) 0);
+        } else {
+            parcel.writeByte((byte) 1);
+            parcel.writeLong(updateDate);
+        }
+        parcel.writeString(ownerId);
+        parcel.writeByte((byte) (deleted ? 1 : 0));
+    }
 
 }
