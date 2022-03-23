10f87164a2d113b9e145355fda390e96e29eedd2
linoybekker <linoylbekker@gmail.com>
2022-03-23 18:21:47 +0200

    Bugfixes and changes DressDescriptionFragment AddDressFragment

--- app/src/main/java/com/example/bride_dresses_project/fragments/dresses/DressDescriptionFragment.java
+++ app/src/main/java/com/example/bride_dresses_project/fragments/dresses/DressDescriptionFragment.java
@@ -22,14 +22,7 @@ import com.google.gson.Gson;
 import com.squareup.picasso.Picasso;
 
 
-public class DressDescriptionFragment extends Fragment {
-
-    public static final String DRESS_PARAM = "Dress";
-    MaterialButton backButton;
-    MaterialButton editButton;
-    MaterialButton deleteButton;
-    private NavController navController;
-
+public class DressDescriptionFragment extends Fragment implements View.OnClickListener {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -40,35 +33,40 @@ public class DressDescriptionFragment extends Fragment {
         TextView dressDesigner = view.findViewById(R.id.dressItemDesigner);
         TextView dressPrice = view.findViewById(R.id.dressItemPrice);
         ImageView dressImage = view.findViewById(R.id.dressItemImage);
-        editButton = view.findViewById(R.id.dress_description_page_edit_button);
-        deleteButton = view.findViewById(R.id.dress_description_page_delete_button);
-        backButton = view.findViewById(R.id.dress_description_back_button);
-        navController = NavHostFragment.findNavController(this);
+        view.findViewById(R.id.dress_description_page_edit_button).setOnClickListener(this);
+        view.findViewById(R.id.dress_description_page_delete_button).setOnClickListener(this);
+        view.findViewById(R.id.dress_description_back_button).setOnClickListener(this);
         dressType.setText(dress.getType());
         dressPrice.setText(dress.getPrice());
-        // dressDesigner.setText(mDress.getUser().getFullName()); TODO
         Picasso.get().load(dress.getImageUrl()).into(dressImage);
-        setDeleteButtonOnClickListener();
-//        setEditButtonOnClickListener();
         return view;
     }
 
 
-    private void setDeleteButtonOnClickListener() {
-        deleteButton.setOnClickListener(view -> {
-            editButton.setEnabled(false);
-            deleteButton.setEnabled(false);
-            new Thread(() -> {
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.dress_description_page_edit_button: {
                 Dress dress = DressDescriptionFragmentArgs.fromBundle(getArguments()).getDress();
-                AppLocalDb.db.dressDao().delete(dress);
-                Model.instance.deleteDress(dress, () -> navController.navigateUp());
-            }).start();
-        });
+                NavHostFragment.findNavController(this).navigate(DressDescriptionFragmentDirections.actionDressDescriptionFragmentToCreateDressFragment3(dress));
+                break;
+            }
+            case R.id.dress_description_page_delete_button: {
+                new Thread(() -> {
+                    Dress dress = DressDescriptionFragmentArgs.fromBundle(getArguments()).getDress();
+                    AppLocalDb.db.dressDao().delete(dress);
+                    Model.instance.deleteDress(dress, () -> navigateUp());
+                }).start();
+                break;
+            }
+            case R.id.dress_description_back_button: {
+                navigateUp();
+                break;
+            }
+        }
     }
-    private void setEditButtonOnClickListener() {
-        Dress editDress = AddDressFragmentArgs.fromBundle(getArguments()).getEditDress();
-        editButton.setOnClickListener(view -> {
-            navController.navigate(DressDescriptionFragmentDirections.actionDressDescriptionFragmentToCreateDressFragment3(editDress));
-        });
+
+    private void navigateUp() {
+        NavHostFragment.findNavController(this).navigateUp();
     }
 }
